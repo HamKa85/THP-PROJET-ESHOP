cb6143d0c332b135578b7ae663fb367fb0491e89
« Thibaut » <gorioux.thibaut@gmail.com>
2018-03-07 11:33:13 +0100

    modification mise en page + organisation controller cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,34 +1,35 @@
 class CartsController < ApplicationController
+  before_action :require_login
+
   def addtocart
-    if user_signed_in?
-      @item=Item.find(params[:id])
       if current_user.cart
-        current_user.cart.items<<@item
-        redirect_to showcart_path
+         current_user.cart.items << Item.find(params[:id])
+         redirect_to showcart_path
       else
-      @cart=Cart.new
-      @cart.user=current_user
-      @cart.items<<@item
-      @cart.save
+      cart=Cart.new
+      cart.user=current_user
+      cart.items<<@item
+      cart.save
       redirect_to showcart_path
       end
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   def show
-    if user_signed_in?
      @cart=current_user.cart.items
-    else
-     redirect_to root_path
-    end
   end
 
-def destroy
-  @item = Item.find(params[:id])
-  @item.destroy
-  redirect_to showcart_path
-end
+  def destroy
+    current_user.cart.items.clear
+    redirect_to showcart_path
+  end
+
+  private
+
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "Vous devez vous connecter pour accéder à votre panier"
+      redirect_to new_user_session_path
+    end
+  end
 
 end
