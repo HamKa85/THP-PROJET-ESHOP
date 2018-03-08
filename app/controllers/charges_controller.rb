ab5304e4a1dd261d811b3e59604a9b35794c0a84
Kyodex <35172439+Kyodex@users.noreply.github.com>
2018-03-08 17:52:46 +0100

    Update charges_controller.rb

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,12 +1,11 @@
 class ChargesController < ApplicationController
   def new
-    @order = Order.find(params[:id])
-    @order.statut = true
-    @order.save!
+    @order = Order.find(params[:id])    
   end
 
   def create
     @amount = (Order.find(params[:id]).total_price*100).round(0)
+    @order = Order.find(params[:id])
 
     customer = Stripe::Customer.create(
     email: params[:stripeEmail],
@@ -19,7 +18,10 @@ class ChargesController < ApplicationController
     description: 'Rails Stripe customer',
     currency: 'usd'
     )
-
+    
+    @order.statut = true
+    @order.save!
+    
     rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
